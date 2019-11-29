97211bc29bca9be88706651c748f7fe60c2e3912
Angelo de Jesus Loza Martinez <angelo.loza@cominvi.local>
2019-11-29 13:13:43 -0600

    [REF]: Se agrega el limpiador de archivos

---
+++ src/main/java/com/cominvi/app/generador/Cleaner.java
@@ -0,0 +1,9 @@
+package com.cominvi.app.generador;
+
+import com.cominvi.app.generador.files.FileCreator;
+
+public class Cleaner {
+	public static void main(String [] a) {
+		FileCreator.deleteAll();
+	}
+}
